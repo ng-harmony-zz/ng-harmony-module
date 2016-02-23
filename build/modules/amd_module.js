3880ae3e05bd56c707cca5944c12181f0c92ba44
DI (FH) Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2016-02-23 16:20:07 +0100

    Bump ... build files uploaded

---
+++ build/modules/amd_module.js
@@ -0,0 +1,51 @@
+import angular from "angular";
+import router from "angular-ui-router";
+import { Ctrl } from "ng-harmony/ng-harmony";
+
+export class Module {
+    constructor(name, dependencies, routes) {
+        this.name = name;
+        this.dependencies = dependencies;
+        this.module = angular.module(name, dependencies);
+        if (typeof routes !== "undefined" && routes !== null) {
+            this.routing(routes);
+        }
+    }
+    bootstrap() {
+        angular.element(document).ready(() => {
+            angular.bootstrap(document, [this.name]);
+        });
+    }
+    routing(routes) {
+        this.routes = routes;
+        this.module.config(($stateProvider, $urlRouterProvider) => {
+            if (typeof routes.default !== "undefined" && routes.default !== null) {
+                $urlRouterProvider.otherwise(routes.default);
+            }
+            for (let [i, route] of Object.getOwnPropertyNames(routes).entries()) {
+                let resolve = {},
+                    state = routes[route];
+                if (typeof state.resolve !== "undefined" && state.resolve !== null) {
+                    for (let [j, stateParam] of Object.getOwnPropertyNames(state.resolve).entries()) {
+                        resolve[stateParam] = ["$stateParams", (_stateParam => {
+                            return $params => {
+                                return $params[state.resolve[_stateParam]];
+                            };
+                        })(stateParam)];
+                    }
+                }
+                let config = state;
+                config.resolve = resolve;
+                $stateProvider.state(route, config);
+            }
+        });
+    }
+    config(foo) {
+        if (typeof this.module === "undefinded" || this.module === null) {
+            throw "Cannot access module: undefined!";
+        }
+        this.module.config(foo);
+    }
+}
+
+//# sourceMappingURL=amd_module.js.map
